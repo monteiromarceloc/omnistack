584267e57674f19b728751a17916606e636dc38c
Marcelo Monteiro <monteiromarceloc@gmail.com>
2020-04-02 16:27:31 -0300

    add validations to routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate')
 const routes = require('./routes');
 
 const app = express();
 app.use(cors()); // { origin: '<url que pode acessar>' }
 app.use(express.json())
 app.use(routes);
+app.use(errors());
 app.listen(3333);
