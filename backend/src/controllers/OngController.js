f96d46d81450177475bccee54d05383f3a90cbf1
Marcelo Monteiro <monteiromarceloc@gmail.com>
2020-04-02 17:40:33 -0300

    first test success

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,5 +1,5 @@
 const connection = require('../database/connection');
-const crypto  = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId')
 
 module.exports = {
   index: async (request, response) => {
@@ -8,7 +8,7 @@ module.exports = {
   },
   create: async (request, response) => {
     const { name, email, whatsapp, city, uf } = request.body;
-    const id = crypto.randomBytes(4).toString('HEX').toUpperCase();
+    const id = generateUniqueId();
     await connection('ongs').insert({ id, name, email, whatsapp, city, uf })
     return response.json({ id });
   },
---
+++ backend/src/tests/unit/generateUniqueId.spec.js
@@ -0,0 +1,5 @@
+describe('Generate Unique ID', () => {
+  it('showld ganerate an unique ID', () => {
+    expect(2 + 2).toBe(4);
+  })
+})
